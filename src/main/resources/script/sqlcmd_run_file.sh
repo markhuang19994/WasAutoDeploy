562073310389ee0cf8ba0b339b8fa8fa5d21a6f8
markhuang1994 <markhuang1993@gmail.com>
2020-04-16 16:53:44 +0800

    [Add]file exist check

--- src/main/resources/script/sqlcmd_run_file.sh
+++ src/main/resources/script/sqlcmd_run_file.sh
@@ -9,7 +9,7 @@ DATABASE=$6
 
 eval "/opt/mssql-tools/bin/sqlcmd -S $HOST,$PORT -U $USER -P $PASSWORD -d $DATABASE -Q 'SELECT 1' -r1 2> $DIR/err_log.txt"
 test_err_log=$(cat "$DIR/test_err_log.txt")
-if [[ $(echo "$test_err_log" | wc -m ) -gt 0 ]]
+if [[ -f "$test_err_log" ]] && [[ $(echo "$test_err_log" | wc -m ) -gt 0 ]]
 then
   exit 1
 fi
