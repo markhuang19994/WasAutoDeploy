7db422e66a083f21ef43562cab788e4d7733bcbc
mark <markhuang1993@gmail.com>
2020-04-21 20:28:11 +0800

    [optimise]run sqlcmd script

--- src/main/resources/script/sqlcmd_run_file.sh
+++ src/main/resources/script/sqlcmd_run_file.sh
@@ -7,25 +7,60 @@ USER=$4
 PASSWORD=$5
 DATABASE=$6
 
-eval "/opt/mssql-tools/bin/sqlcmd -S $HOST,$PORT -U $USER -P $PASSWORD -d $DATABASE -Q 'SELECT 1' -r1 2> $DIR/err_log.txt"
-test_err_log=$(cat "$DIR/test_err_log.txt")
-if [[ -f "$test_err_log" ]] && [[ $(echo "$test_err_log" | wc -m ) -gt 0 ]]
+function is_file_has_content() {
+  FILE_PATH=$1
+  if [[ -f "$FILE_PATH" ]]
+  then
+    content=$(cat "$FILE_PATH")
+    if [[ $(echo "$content" | wc -m ) -gt 1 ]]
+    then
+      return 1
+    fi
+  fi
+  return 0
+}
+
+echo 'Test connection'
+TEST_ERROR_LOG_PATH="$DIR/test_err_log.txt"
+eval "/opt/mssql-tools/bin/sqlcmd -S $HOST,$PORT -U $USER -P $PASSWORD -d $DATABASE -Q 'SELECT 1' -r1 2> $TEST_ERROR_LOG_PATH"
+
+is_file_has_content "$TEST_ERROR_LOG_PATH"
+if [[ "$?" -eq 1 ]]
 then
+  echo 'Test error:'
+  cat "$TEST_ERROR_LOG_PATH"
   exit 1
 fi
 
-INSTRCTION_FILE=$(cat "$DIR/instruct.txt")
-for (( i = 1; i <= $(echo "$INSTRCTION_FILE" | wc -l); i++ ))
+INSTRUCTION_FILE=$(cat "$DIR/instruct.txt")
+for (( i = 1; i <= $(echo "$INSTRUCTION_FILE" | wc -l); i++ ))
 do
-  sqlFileName="$(echo "$INSTRCTION_FILE" | sed -n "${i}p")"
+  sqlFileName="$(echo "$INSTRUCTION_FILE" | sed -n "${i}p")"
   if [[ -z $sqlFileName ]]
   then
     continue
   fi
+
   sqlFilePath="$DIR/$sqlFileName"
-  cmd="/opt/mssql-tools/bin/sqlcmd -S $HOST,$PORT -U $USER -P $PASSWORD -d $DATABASE -i '$sqlFilePath' -r1 2> $DIR/err_log.txt"
+  ERROR_LOG_PATH="$DIR/err_log_$i.txt"
+  SUCCESS_LOG_PATH="$DIR/suc_log_$i.txt"
+  cmd="/opt/mssql-tools/bin/sqlcmd -S $HOST,$PORT -U $USER -P $PASSWORD -d $DATABASE -i '$sqlFilePath' -r0 2>$ERROR_LOG_PATH 1>$SUCCESS_LOG_PATH"
   echo "cmd:$cmd"
   eval "$cmd"
-  cat  "$DIR/err_log.txt"
-  echo "run $sqlFilePath success."
+
+  is_file_has_content "$ERROR_LOG_PATH"
+  if [[ "$?" -eq 1 ]]
+  then
+      echo "Run $sqlFilePath error:"
+    cat "$ERROR_LOG_PATH"
+    exit 1
+  fi
+
+  is_file_has_content "$SUCCESS_LOG_PATH"
+  if [[ "$?" -eq 1 ]]
+  then
+    cat "$SUCCESS_LOG_PATH"
+  fi
+  echo "Run $sqlFilePath success."
 done
+exit 0
